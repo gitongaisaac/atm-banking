3c936128f3e472e31b7f697ff23bc81a3e549ed7
gitongaisaac <isaacggitonga@gmail.com>
2023-07-22 00:33:58 +0300

    A commit

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,13 @@
-import com.sun.source.tree.WhileLoopTree;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import static java.lang.Class.forName;
-
 public class Test {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/birthdays";
-        String username = "root";
-        String password = "My5q! 53rv3r";
+        String url = "jdbc:mysql://127.0.0.1:3306/atm_banking";
+        String username = "isaac";
+        String password = "D3F 15@@c 53rv3r";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -20,12 +16,12 @@ public class Test {
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from dinners");
+            ResultSet resultSet = statement.executeQuery("SELECT SSN, last_name, first_name, email_address, state FROM Person_info");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(1) + " " + resultSet.getDate(2) +
-                        " " + resultSet.getString(3) + " " + resultSet.getString(4) +
-                        " " + resultSet.getString(5));
+                System.out.println(resultSet.getString(1) + "    **********    " + resultSet.getString(2) +
+                        "    *********     " + resultSet.getString(3) + "    *********    " + resultSet.getString(4) +
+                        "    *********     " + resultSet.getString(5));
             }
 
             connection.close();
